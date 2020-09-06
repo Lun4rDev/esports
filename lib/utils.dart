4c304e7087d17d6748576a8228fb6d9243180be3
Mickaël Hernandez <mickael.hernandez9@gmail.com>
2020-09-06 14:26:58 +0200

    caching network images + shimmer animation

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,12 +1,23 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:esports/localizations.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 // i18n String getter
   String str(BuildContext context, String key) => EsportsLocalizations.of(context).get(key);
 
   // Sized square image fetched from the web
   Widget image(String url, double size) => url != null 
-    ? SizedBox(width: size, height: size, child: Image.network(url),) 
+    ? SizedBox(width: size, height: size, child: CachedNetworkImage(imageUrl: url,
+      progressIndicatorBuilder: (context, _, __) => Shimmer.fromColors(
+        baseColor: Colors.transparent,
+        highlightColor: Colors.white12,
+        child: Container(
+          margin: EdgeInsets.all(2),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(16))),)
+      ),),) 
     : SizedBox(width: size, height: size);
 
   // Loading animation
