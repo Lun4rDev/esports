fb1e3eb041037ea4cdb659c016e50ece0bcd5844
Mickaël Hernandez <mickael.hernandez9@gmail.com>
2020-09-06 12:15:21 +0200

    extracting match and tournament sheet into widgets

--- lib/tabs/tournaments/open_roster.dart
+++ lib/tabs/tournaments/open_roster.dart
@@ -3,7 +3,7 @@ import 'package:esports/model/tournamentmodel.dart';
 import 'package:esports/utils.dart';
 import 'package:flutter/material.dart';
 
-Future<ExpectedRoster> downloadRoster(
+Future<void> downloadRoster(
   BuildContext context, 
   int tournamentId, 
   int rosterId) async {
@@ -21,36 +21,46 @@ openRoster(BuildContext context, ExpectedRoster roster) async {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.vertical(top: Radius.circular(16.0))),
       builder: (context) {
-        return Container(
-          margin: EdgeInsets.all(16),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-            image(roster.team.imageUrl, 80),
-            SizedBox(height: 4),
-            Text(roster.team.name, style: TextStyle(fontSize: 24)),
-            SizedBox(height: 26, child: Divider()),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(children: <Widget>[
-                for(var player in roster.players)
-                  ...[
-                    Column(
-                      children: <Widget>[
-                        image(player.imageUrl, 100),
-                        SizedBox(height: 4,),
-                        if(player.role != null) Text(player.role),
-                        Text(player.name, style: TextStyle(fontSize: 20)),
-                        if(player.firstName != null) Text("${player.firstName} ${player.lastName}"),
-                        if(player.hometown != null) Text(player.hometown, style: TextStyle(fontSize: 16)),
-                      ],
-                    ),
-                    SizedBox(width: 16,)
-                  ]
-              ]),
-            )
-          ],),
-        );
+        return TournamentSheet(roster);
       }
     );
   }
+
+class TournamentSheet extends StatelessWidget {
+  const TournamentSheet(this.roster, {Key key}) : super(key: key);
+
+  final ExpectedRoster roster;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(16),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+        image(roster.team.imageUrl, 80),
+        SizedBox(height: 4),
+        Text(roster.team.name, style: TextStyle(fontSize: 24)),
+        SizedBox(height: 26, child: Divider()),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(children: <Widget>[
+            for(var player in roster.players)
+              ...[
+                Column(
+                  children: <Widget>[
+                    image(player.imageUrl, 100),
+                    SizedBox(height: 4,),
+                    if(player.role != null) Text(player.role),
+                    Text(player.name, style: TextStyle(fontSize: 20)),
+                    if(player.firstName != null) Text("${player.firstName} ${player.lastName}"),
+                    if(player.hometown != null) Text(player.hometown, style: TextStyle(fontSize: 16)),
+                  ],
+                ),
+                SizedBox(width: 16,)
+              ]
+          ]),
+        )
+      ],),
+    );
+  }
+}
